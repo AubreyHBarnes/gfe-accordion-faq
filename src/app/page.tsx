12667d84f55dfec7fb5bd6867d4558dbaedd01e2
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2025-06-29 17:28:29 -0500

    layout and width adjustments so things don't get too wide and remain centered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { CTACard } from "./components/CTACard";
 export default async function Home() {
   
   return (
-    <div className="grid items-center gap-12 justify-items-center p-4 md:p-8 pb-20 sm:p-20 font-[family-name:var(--font-noto-sans)]">
+    <div className="max-w-[1200px] m-auto grid items-center gap-12 justify-items-center p-4 md:p-8 pb-20 sm:p-20 font-[family-name:var(--font-noto-sans)]">
       <section className="intro-copy-container flex flex-col items-center gap-4">
         <h1 className="intro-subhead text-3xl text-center font-semibold">Frequently Asked Questions</h1>
         <h2 className="intro-heading text-center ">Get All Your Questions Answered</h2>
@@ -13,7 +13,7 @@ export default async function Home() {
       <section className="w-full">
         <Accordion />
       </section>
-      <footer>
+      <footer className="w-full">
         <CTACard heading="Can’t find the answer you’re looking for?" text="Reach out to our customer support team." buttonLabel="Get In Touch"/>
       </footer>
     </div>
